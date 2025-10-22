d9f7df95c09ff3d0fcfeece4d697249de713edbf
Sebastian <sebashm68@gmail.com>
2025-10-21 22:51:31 -0600

    cambia nombres de barberos en db

--- src/data/db.js
+++ src/data/db.js
@@ -1,10 +1,10 @@
 export const users = [
-    { name: 'BarberoA'},
-    { name: 'BarberoB'},
-    { name: 'BarberoC'},
-    { name: 'BarberoD'},
-    { name: 'BarberoE'},
-    { name: 'BarberoF'},
-    { name: 'BarberoG'},
-    { name: 'BarberoH'},
+    { name: 'BarberoA-3'},
+    { name: 'BarberoB-3'},
+    { name: 'BarberoC-3'},
+    { name: 'BarberoD-3'},
+    { name: 'BarberoE-3'},
+    { name: 'BarberoF-3'},
+    { name: 'BarberoG-3'},
+    { name: 'BarberoH-3'},
 ]
